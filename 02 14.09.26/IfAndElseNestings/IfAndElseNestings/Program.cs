b48555fb27b848dcb89a52fd84a18658eb925b05
Opilane Techno+TLN <bulskidaniel023@gmail.com>
2026-09-14 11:44:40 +0300

    Ifelsenesting

---
+++ 02 14.09.26/IfAndElseNestings/IfAndElseNestings/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace IfAndElseNestings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //muutja nimega y on double e
+            //komakohaga arv ja väärus on 9
+            double y = 9.0;
+            if (y == 9)
+            {
+                if (y == 11)
+                {
+                    Console.WriteLine("Vastus on 11");
+                }
+                else
+                {
+                    Console.WriteLine("Vastus on kõik peale 11");
+                }
+            }
+            else if (y == 30)
+
+            {
+                Console.WriteLine("Vastus on 30");
+            }
+            else
+            {
+                Console.WriteLine("Mingi kahtlane number");
+            }
+        }
+    }
+}
+
+
