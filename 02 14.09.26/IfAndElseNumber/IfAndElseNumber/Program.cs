45ea654c907ea7260b91d06619d35ac939b33815
Opilane Techno+TLN <bulskidaniel023@gmail.com>
2026-09-14 11:03:24 +0300

    If12

---
+++ 02 14.09.26/IfAndElseNumber/IfAndElseNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace IfAndElseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta enda vanus");
+
+            //peate katsuma if and else lauseid,
+            //et kontrollida, kas kasutaja vanus
+            //on suurem kui 18 või väiksem kui 18
+
+            string sisend = Console.ReadLine();
+            int vanus = int.Parse(sisend);
+
+            if(vanus > 18)
+                 {
+                Console.WriteLine("Sinu vanus on suurem kui 18");
+            }
+            else if (vanus < 18)
+            {
+                Console.WriteLine("Sinu vanus on väiksem kui 18");
+            }
+            else
+            {
+                Console.WriteLine("Sinu vanus on täpselt 18");
+            }
+        }
+    }
+}
+
+
---
+++ 02 14.09.26/IfAndelse/IfAndelse/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace IfAndelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Siseta enda nimi!");
+            Console.WriteLine("https://meet.google.com/mqx-qqdo-ncp?pli=1&authuser=2");
+            //siin on muutja nimega nime,
+            //mis on tüübiga string
+            //loeb andmeid konsoolist ja salvestab
+            //need muutja nime sisse
+            string name = Console.ReadLine();
+
+            //if ja else kontrollib, kas muutja
+            //name on tühi või mitte
+            //kui muutja name on tühi, siis väljastab konsoolile
+            //teksti "Tere,tundmatu! ERROR ja teeb 4 piiksu
+            if (name != "")
+            {
+                Console.WriteLine("Tere, " + name);
+                
+            }
+            else
+            {
+                Console.WriteLine("Tere, tundmatu!ERROR");
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+                Console.Beep();
+                Thread.Sleep(1000);
+
+
+
+            }
+;
+
+        }
+    }
+}
